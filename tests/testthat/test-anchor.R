fe7f289f92d3591d9be4ac57b8ea529b25dea295
markus <markus_ulmer@bluewin.ch>
2025-02-12 11:17:10 +0100

    cleanup

--- tests/testthat/test-anchor.R
+++ tests/testthat/test-anchor.R
@@ -5,5 +5,5 @@ X <- matrix(rnorm(n * 20), nrow = n)
 Y <- rnorm(n)
 A <- rnorm(n)
 
-SDForest(x = X, y = Y, A = A, Q_type = 'no_deconfounding')
-SDForest(x = X, y = Y, A = A)
+expect_no_error(SDForest(x = X, y = Y, A = A, Q_type = 'no_deconfounding', nTree = 2))
+expect_no_error(SDForest(x = X, y = Y, A = A, nTree = 2))
--- tests/testthat/test-stratForest.R
+++ tests/testthat/test-stratForest.R
@@ -1,11 +1,18 @@
 set.seed(1)
 
 n <- 100
-X <- matrix(rnorm(n * 20), nrow = n)
+X <- matrix(rnorm(n * 4), nrow = n)
 Y <- X[, 1] * rnorm(n)
 A <- sample(1:3, n, prob = c(0.3, 0.3, 0.3), replace = TRUE)
 A <- as.factor(A)
 
-SDForest(x = X, y = Y, envs = A, nTree_leave_out = 5, cp = 0.1)
-SDForest(x = X, y = Y, envs = A, nTree_env = 5, cp = 0.1)
-SDForest(x = X, y = Y, envs = A, nTree_leave_out = c('1' = 0, '2' = 3, '3' = 8), cp = 0.1)
+fit1 <- SDForest(x = X, y = Y, envs = A, nTree_leave_out = 5, cp = 0.1, verbose = FALSE)
+fit2 <- SDForest(x = X, y = Y, envs = A, nTree_env = 5, cp = 0.1, verbose = FALSE)
+fit3 <- SDForest(x = X, y = Y, envs = A, nTree_leave_out = c('1' = 0, '2' = 3, '3' = 8), 
+                 cp = 0.1, verbose = FALSE)
+
+expect_equal(length(fit1$forest), 5 * length(unique(A)))
+expect_equal(length(fit2$forest), 5 * length(unique(A)))
+expect_equal(length(fit3$forest), sum(c('1' = 0, '2' = 3, '3' = 8)))
+
+
